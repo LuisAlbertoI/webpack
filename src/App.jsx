433dc0a9a73a0827648365deb5261a613be32164
Luis Alberto I <alberdj987@gmail.com>
2019-04-27 14:57:04 -0500

    'add-proyect'

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+
+const App = () =>(
+    <div className="App">
+        <div className="App-Container">
+            <div className="App-Loader"></div>
+            <span>Welcome To React</span>
+        </div>
+    </div>
+);
+
+export default App;
